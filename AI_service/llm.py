138943e6c621c5898022060822fa2f6619216201
HuynhGB <131958958+HuynhGB@users.noreply.github.com>
2025-05-08 21:36:25 +0700

    Update llm.py

--- AI_service/llm.py
+++ AI_service/llm.py
@@ -9,20 +9,16 @@ CORS(app)
 ollama_url = os.getenv("OLLAMA_BASE_URL", "http://localhost:11434")
 llm = Ollama(base_url=ollama_url, model="tinyllama")
 
-
-
 @app.route('/api/process', methods=['POST'])
 def process_message():
-    global answer
     data = request.get_json()
     if data and 'message' in data:
         received_message = data['message']
         result = llm(received_message)
-        res1 = result.split('\n')
-        answer = res1
-        return jsonify({"received_message": res1})
+        response_lines = result.split('\n')
+        return jsonify({"received_message": response_lines})
     else:
         return jsonify({"error": "Invalid Data"}), 400
-    
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
