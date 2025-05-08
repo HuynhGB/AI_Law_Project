7efb410be7e2661248a7e59627046d83a5e9131a
HuynhGB <131958958+HuynhGB@users.noreply.github.com>
2025-05-08 21:41:56 +0700

    Update script.js

--- frontend_service/script.js
+++ frontend_service/script.js
@@ -3,8 +3,8 @@ const question_1 = document.getElementById("question_1");
 const AI_Answer = document.getElementById("result");
 const modified_AI_Answer = document.getElementById("modified_result");
 
-function adjust_the_question(){modified_question_1.value =  question_1.textContent;}
-function adjust_the_answer(){modified_AI_Answer.value = AI_Answer.textContent;}
+function adjust_the_question(){ modified_question_1.value = question_1.textContent; }
+function adjust_the_answer(){ modified_AI_Answer.value = AI_Answer.textContent; }
 
 document.addEventListener('DOMContentLoaded', function() {
     const send_to_AI = document.getElementById('button1');
@@ -14,43 +14,23 @@ document.addEventListener('DOMContentLoaded', function() {
 
         fetch('http://74.208.88.183:5000/api/process', {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ message: message })
         })
         .then(response => response.json())
         .then(data => {
-            console.log('Phản hồi từ backend (process):', data);
             AI_Answer.innerHTML = '';
-            const responseItem = document.createElement('li');
-            responseItem.textContent = data.received_message;
-            AI_Answer.appendChild(responseItem);
+            data.received_message.forEach(line => {
+                const li = document.createElement('li');
+                li.textContent = line;
+                AI_Answer.appendChild(li);
+            });
         })
         .catch(error => {
-            console.error('Lỗi khi gửi dữ liệu (process):', error);
             AI_Answer.innerHTML = '';
             const errorItem = document.createElement('li');
             errorItem.textContent = 'Không thể gửi dữ liệu hoặc nhận phản hồi.';
             AI_Answer.appendChild(errorItem);
         });
     });
-    function fetchData(){
-        fetch('http://74.208.88.183:5000/api/process')
-                .then(response => response.json())
-                .then(data => {
-                  const list = document.getElementById('result');
-                  list.textContent = '';
-                  data.forEach(value => {
-                  const li = document.createElement('li');
-                  li.textContent = value.replace(/\*/g, '');
-                  list.appendChild(li);
-                  });
-                })
-                .catch(error => console.error('Lỗi:', error));
-      }
-      
-    fetchData();
-    //setInterval(fetchData, 3000);
-
 });
